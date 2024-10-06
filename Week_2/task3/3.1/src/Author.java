9d8cee475b22e9cab3c1fcbb5a676479aee49ec9
Pepega-1984 <alexandr.minyaev@gmail.com>
2024-10-06 03:03:02 +0300

    Add files via upload

---
+++ Week_2/task3/3.1/src/Author.java
@@ -0,0 +1,26 @@
+package org.example;
+
+
+public class Author {
+    private String name;
+    private int birthYear;
+    public Author(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getBirthYear() {
+        return birthYear;
+    }
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+    public String toString() {
+        return String.format("Автор: %s (%d года рождения)", name, birthYear);
+    }
+}
---
+++ Week_2/task3/3.1/src/Book.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Book {
+    private Author author;
+    private String title;
+    public Book(Author author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String toString() {
+        return String.format("Книга \"%s\", %s", title, author.toString());
+    }
+}
